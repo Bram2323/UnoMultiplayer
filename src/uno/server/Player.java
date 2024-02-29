927576a00409994a4304bfc1a6507a5f77a2c3c4
Vexarian <julianpasker@gmail.com>
2024-02-29 14:28:57 +0100

    Added constructor to player

--- src/uno/server/Player.java
+++ src/uno/server/Player.java
@@ -6,6 +6,22 @@ import java.util.ArrayList;
 
 public class Player {
 
+    private final int id;
+    private final String name;
+
+    public Player(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
     ArrayList<Card> cardsInHand = new ArrayList<>();
 
     public void addCard(Card card) {
