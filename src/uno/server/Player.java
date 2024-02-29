e86fda8b323157776e8abab7afb5b2385844f5d2
Vexarian <julianpasker@gmail.com>
2024-02-29 13:49:00 +0100

    Finished player class

--- src/uno/server/Player.java
+++ src/uno/server/Player.java
@@ -2,12 +2,25 @@ package uno.server;
 
 import uno.common.cards.Card;
 
+import java.util.ArrayList;
+
 public class Player {
 
+    ArrayList<Card> cardsInHand = new ArrayList<>();
 
-    public void addCard(Card card) {}
+    public void addCard(Card card) {
+        cardsInHand.add(card);
+    }
 
-    public void removeCard(Card card) {}
+    public void removeCard(Card card) {
+        cardsInHand.remove(card);
+    }
 
-    public boolean hasCard(Card card) {return false;}
+    public boolean hasCard(Card card) {
+        return (!cardsInHand.isEmpty());
+    }
+
+    public ArrayList<Card> getCardsInHand() {
+        return new ArrayList<>(cardsInHand);
+    }
 }
